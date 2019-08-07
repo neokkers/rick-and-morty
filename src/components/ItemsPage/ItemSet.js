168a8eea24325de8ca0cde59f4a48cd0160b20c8
Neokkers <mendelson.inbox@gmail.com>
2019-08-07 19:28:43 +0300

    loader

--- src/components/ItemsPage/ItemSet.js
+++ src/components/ItemsPage/ItemSet.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ItemCard from './ItemCard';
+import Loader from '../Loader/Loader';
 
-const ItemSet = ({ items }) => (
-  <div
-    style={{
-      display: 'grid',
-      gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
-      gridGap: '20px',
-      width: '1300px',
-      margin: '0 auto',
-      padding: '30px 0',
-    }}
-  >
-    {items.map(el => (
-      <ItemCard key={el.id} item={el} />
-    ))}
-  </div>
-);
+const ItemSet = ({ items, loading }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
+        gridGap: '20px',
+        width: '1300px',
+        margin: '0 auto',
+        padding: '30px 0',
+      }}
+    >
+      {items.map(el => (
+        <ItemCard key={el.id} item={el} />
+      ))}
+    </div>
+  );
 
 export default ItemSet;
--- src/components/ItemsPage/ItemsPage.js
+++ src/components/ItemsPage/ItemsPage.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { getItemsByPage } from '../../services/apiService';
 import Pagination from './Pagination';
 import ItemSet from './ItemSet';
+import Loader from '../Loader/Loader';
 
 export default class ItemsPage extends Component {
+  state = {
+    loading: false,
+  };
+
   async componentDidMount() {
     this.updateCurrentTab();
     this.getAndSetData('mount'); // method without updating data (for safety when route changes)
@@ -30,17 +35,20 @@ export default class ItemsPage extends Component {
   };
 
   getAndSetData = async type => {
+    await this.setState({ loading: true });
     // eslint-disable-next-line prettier/prettier
     const { match: { params: { itemType, pageNumber } }, setData, data } = this.props;
     if (type === 'mount' && data.results) return;
     const res = await getItemsByPage(itemType.slice(0, -1), pageNumber);
-    setData(itemType, res.data, pageNumber);
+    await setData(itemType, res.data, pageNumber);
+    this.setState({ loading: false });
   };
 
   render() {
     // eslint-disable-next-line prettier/prettier
     const { match: { params: { itemType, pageNumber } }, data } = this.props;
-    if (!data || !data.info) return null;
+    const { loading } = this.state;
+    if (!data || !data.info) return <Loader />;
 
     return (
       <div style={{ marginTop: '20px' }}>
@@ -49,7 +57,7 @@ export default class ItemsPage extends Component {
           pageCount={data.info.pages}
           pageNumber={pageNumber}
         />
-        <ItemSet items={data.results} />
+        <ItemSet items={data.results} loading={loading} />
       </div>
     );
   }
