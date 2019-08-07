1385296da4262aa340ca9fc22f0c870eb11dd652
Neokkers <mendelson.inbox@gmail.com>
2019-08-07 15:06:45 +0300

    minor

--- src/components/ItemsPage/ItemSet.js
+++ src/components/ItemsPage/ItemSet.js
@@ -5,7 +5,7 @@ const ItemSet = ({ items }) => (
   <div
     style={{
       display: 'grid',
-      gridTemplateColumns: '1fr 1fr 1fr',
+      gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
       gridGap: '20px',
       width: '1300px',
       margin: '0 auto',
